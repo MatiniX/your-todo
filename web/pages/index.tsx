2d6e3590df7702c913e97e5bdbccdd74e71f14ad
MatiniX <mato.michalik10@gmail.com>
2022-01-16 11:18:25 +0100

    Frontend setup

---
+++ web/pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return <h1 className="inline-block mx-auto text-9xl font-black">Index page</h1>;
+};
+
+export default Home;
