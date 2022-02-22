c87e7b68a152a45ac8294b0428f336be9f5034f6
MatiniX <mato.michalik10@gmail.com>
2022-02-22 19:49:18 +0100

    Nastavenie proxy

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,15 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { __prod__ } from './constants';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const configService = app.get(ConfigService);
 
+  app.set('trust proxy', 1);
   app.enableCors({
     credentials: true,
     origin: configService.get('CORS_ORIGIN'),
